34748d032143be1fa6ce54492995c54f974d1182
Rocío Bustos <ritarociobustos@gmail.com>
2021-11-17 08:28:55 -0300

    Modulo Compras - cliente implementado en Angular

--- Compras/ComprasRest/app.js
+++ Compras/ComprasRest/app.js
@@ -50,6 +50,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//CORS
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use('/', indexRouter);
 //PROBANDO
 app.use(require('./src/controllers/probando'));
